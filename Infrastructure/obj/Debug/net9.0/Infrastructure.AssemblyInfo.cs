85fede24a78c00d1016deb01035609ac5754e0d5
Eric Hoover <ehoover3@yahoo.com>
2024-11-28 15:27:01 -0500

    clean up program class

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4844f4cbeebe9e2090c0cbb401d54d2b6de6846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d791a15228064b05c973b72194efaab25ecfe60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
