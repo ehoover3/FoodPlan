9a07b94caae0cb713856889e155152cc6a013c98
Eric Hoover <ehoover3@yahoo.com>
2024-11-27 21:43:18 -0500

    restart project, add ski images

--- src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { FoodInventoryComponent } from './inventory/inventory.component';
-
-export const routes: Routes = [{ path: '', component: FoodInventoryComponent }];
